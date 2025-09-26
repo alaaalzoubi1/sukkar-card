f4b80057d4a2196fe3c580bd191f9bc29de9be01
alaaalzoubi1 <119401968+alaaalzoubi1@users.noreply.github.com>
2025-09-27 01:12:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener("load", () => {
+  setTimeout(() => {
+    const loading = document.getElementById("loading");
+    loading.classList.add("fade-shrink"); // start animation
+    loading.addEventListener("animationend", () => {
+      loading.style.display = "none";
+      document.getElementById("card").classList.remove("hidden");
+    });
+  }, 3000); // 3 seconds loading
+});
